67db7b57c7cd0bbbaf035ce4bc04c2440b2e00ee
Ertyom <110183848+Ertyom@users.noreply.github.com>
2024-05-24 12:37:42 +0300

    Working on the account_page.

--- my_auth_site/settings.py
+++ my_auth_site/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sign_up',
-    'sign_in',
-    'account_page',
+    #'sign_up',
+    #'sign_in',
+    #'account_page',
+    'auth',
 ]
 
 MIDDLEWARE = [
