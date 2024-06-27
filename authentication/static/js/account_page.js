1f7823a8d9441430fbeb89f1687356ef797db458
Ertyom <110183848+Ertyom@users.noreply.github.com>
2024-06-27 20:18:47 +0300

    Working on the account_page

---
+++ authentication/static/js/account_page.js
@@ -0,0 +1,33 @@
+function chooseFile() {
+    document.getElementById("profile-picture-input").click();
+}
+
+function submitForm() {
+    document.getElementById("profile-picture-form").submit();
+}
+
+function showContainer(containerId) {
+    var containers = document.querySelectorAll('.Information-Container');
+    containers.forEach(function(container) {
+        if (container.id === containerId) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showContainer('account-container');
+
+    const buttons = document.querySelectorAll('.Main-Button');
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            buttons.forEach(btn => {
+                btn.classList.remove('active');
+            });
+
+            this.classList.add('active');
+        });
+    });
+});
